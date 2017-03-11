5b3aa666c166dc70bae4bbcb4faadc55aebb721b
JANDERSON-PC\jande <janderson.rdsilva@gmail.com>
2017-03-11 13:08:57 -0300

    Aula 14

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ var app = express(); //executa a function retornada pelo express
 app.set('view engine', 'ejs'); //informa ao express quem é o engine de views
 
 app.get('/', function(req, res){
-    res.render('secao/home'); //Renderiza a tela.
+    res.render('home/index'); //Renderiza a tela.
 });
 
-app.get('/vagas', function(req, res){
-    res.send('<html><body><h1>Todas Vagas</h1></body></html>');
+app.get('/formulario_inclusao_noticia', function(req, res){
+    res.render('admin/form_add_noticia'); //Renderiza a tela.
+});
+
+app.get('/noticias', function(req, res){
+    res.render('noticias/noticias'); //Renderiza a tela.
 });
 
 /**
