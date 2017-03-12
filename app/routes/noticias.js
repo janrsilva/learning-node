da9484b670908da80011fa370589ae87b98dd158
JANDERSON-PC\jande <janderson.rdsilva@gmail.com>
2017-03-11 21:48:41 -0300

    Aula 22 Criando conexão com MySQL e recuperando dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,16 @@
 module.exports = function (app) {
+    
     app.get('/noticias', function(req, res){
-        res.render('noticias/noticia'); //Renderiza a tela.
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'portal_noticias'
+        });
+        connection.query('select * from noticias', function (erro, result) {
+            res.send(result); 
+        });
+        // res.render('noticias/noticia'); //Renderiza a tela.
     });
 }
