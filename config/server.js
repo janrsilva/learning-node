a65fb3fc15eab75049f2be49b16771a3b6be7482
JANDERSON-PC\jande <janderson.rdsilva@gmail.com>
2017-03-12 15:51:17 -0300

    Aula 40

--- config/server.js
+++ config/server.js
@@ -1,16 +1,20 @@
 var express = require('express'); // Recupera a bibioteca do express
 var consing = require("consign");
 var bodyParser = require("body-parser");
+var expressValidator = require("express-validator");
 
 var app = express(); //executa a function retornada pelo express
+
 app.set('view engine', 'ejs'); //informa ao express quem é o engine de views
 app.set('views', './app/views');
+
 app.use(bodyParser.urlencoded({extended: true})); //Adiciona bodyParser como middleware
+app.use(expressValidator());
 
 consing()
-.include('./app/routes') //Adiciona a pasta routes
-.then('./config/db.js') //Adiciona a modulo do banco de dados
-.then('./app/models') //Adiciona a models do banco de dados
-.into(app); // Adiciona na instancia do app
+    .include('./app/routes') //Adiciona a pasta routes
+    .then('./config/db.js') //Adiciona a modulo do banco de dados
+    .then('./app/models') //Adiciona a models do banco de dados
+    .into(app); // Adiciona na instancia do app
 
 module.exports = app; //exporta o app
