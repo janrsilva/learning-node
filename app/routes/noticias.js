7a40ea1a660062c8acbbc8078856bb51a97d8b2b
JANDERSON-PC\jande <janderson.rdsilva@gmail.com>
2017-03-11 22:32:35 -0300

    Aula 24

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,13 +1,9 @@
+var dbConnection = require('../../config/db');
+
 module.exports = function (app) {
-    
+    var connection = dbConnection();
+
     app.get('/noticias', function(req, res){
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'portal_noticias'
-        });
         connection.query('select * from noticias', function (erro, result) {
             res.render('noticias/noticia', {noticia: result}); //Renderiza a tela.
         });
