e58ef4fcf3c89c30e100d6f03de280ceff49c5b8
JANDERSON-PC\jande <janderson.rdsilva@gmail.com>
2017-03-15 13:08:14 -0300

    Aula 51

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var app = require("./config/server");
 /**
  * Inicia o servidor na porta 80
  */
-app.listen(8080, function(){ 
+app.listen(80, function(){ 
     console.log('Servidor Iniciado');
 });
